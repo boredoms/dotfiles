638126792c506528d5906ba4c194446cbbaa369a
零 <m.voetsch@gmail.com>
2017-10-02 17:42:58 +0200

    define backup behavior

--- emacs.d/base/yukari-base.el
+++ emacs.d/base/yukari-base.el
@@ -1,3 +1,9 @@
+(setq backup-directory-alist `(("." "~/.emacs.d/backups")))
+(setq backup-by-copying t)
 
+(setq delete-old-versions t
+      kept-new-versions 5
+      kept-old-versions 3
+      version-control t)
 
 (provide 'yukari-base)
